6cc9a2660373c7f2304ee36fc2689893314a6232
Abdoulrahime Diallo <diallo.abdoulrahime@gmail.com>
2018-03-04 06:40:56 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('.target')
+}
+
+function increaseRankBy(n) {
+  const ranks = document.querySelectorAll('ul.ranked-list li')
+ 
+for (let i = 0; i < ranks.length; i++) {
+  ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
+  }
+}
+
+function deepestChild(){
+  var deepest = document.getElementById('grand-node').querySelectorAll('div')
+  return deepest[deepest.length-1]
+}
